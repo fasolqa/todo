915f74035a89b26ca272c0a3a4dc9b5de0ff2316
Krystian Kuzminski <kkuzminski@betacom.com.pl>
2017-04-10 16:19:14 +0200

    progress commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,37 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+
+import { PostsService } from './posts.service';
+
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [PostsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
